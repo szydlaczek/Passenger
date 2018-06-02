7bff52988104a1f0ce7a01f06873af0f9ff0d729
DESKTOP-HTGVKR9\szydl <szydlak@gmail.com>
2018-06-03 00:08:51 +0200

    Create Project

---
+++ Passenger.Core/Domain/DailyRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Passenger.Core.Domain
+{
+    public class DailyRoot
+    {
+        public Guid Id { get; protected set; }
+        public Route Route { get; protected set; }
+        public IEnumerable<PassengerNode> PassengerNodes { get; protected set; }
+    }
+}
---
+++ Passenger.Core/Domain/Driver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Passenger.Core.Domain
+{
+    public class Driver
+    {
+        public Guid Id { get; protected set; }
+        public Guid UserId { get; protected set; }
+        public Vehicle Vehicle { get; protected set; }
+        public IEnumerable<Route> Routes { get; protected set; }
+        public IEnumerable<DailyRoot> DailyRoutes { get; protected set; }
+
+    }
+}
